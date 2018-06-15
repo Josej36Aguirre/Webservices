d34ab6aa81f7f619108d009d015dd3376f20b61a
Josej36Aguirre <josej36.aguirre@gmail.com>
2018-06-14 21:32:12 -0500

    sdfghj

--- Webservices/Webservices/MainPage.xaml.cs
+++ Webservices/Webservices/MainPage.xaml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-
+using Webservices.Clases;
 namespace Webservices
 {
 	public partial class MainPage : ContentPage
@@ -12,6 +12,25 @@ namespace Webservices
 		public MainPage()
 		{
 			InitializeComponent();
+            btn1.Clicked += Btn1_Clicked;
 		}
-	}
+
+        private async void Btn1_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                UserManager manager = new UserManager();
+                var res = await manager.GetUser();
+                if (res != null)
+                {
+                    lstUsuarios.ItemsSource = res;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
 }
