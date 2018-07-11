c404d85c0fe752e2a9f78c3a69eccaa8b000932f
Josej36Aguirre <josej36.aguirre@gmail.com>
2018-07-11 00:37:50 -0500

    terminacion de un web services con codeigniteer

--- Webservices/Webservices/Clases/User.cs
+++ Webservices/Webservices/Clases/User.cs
@@ -6,12 +6,12 @@ namespace Webservices.Clases
 {
     class User
     {
-        public int codigo { get; set; }
+        public string Codigo { get; set; }
         public string Nombre { get; set; }
         public string Marca { get; set; }
-        public decimal Contenido { get; set; }
-        public decimal Valor { get; set; }
-        public decimal Descuento { get; set; }
+        public string Contenido { get; set; }
+        public string Valor { get; set; }
+        public string Descuento { get; set; }
         public string Imagen { get; set; }
     }
 }
